d068fa5fb8b3597e34680e5f57d952879ab4bf60
Wilfried Kopp <chevdor@gmail.com>
2014-08-24 03:34:13 +0200

    Fixing the tests to get at least a test that means a little something.

--- test/test-creation.js
+++ test/test-creation.js
@@ -3,6 +3,27 @@
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 
+var testData = {
+  githubUser         : 'chevdor',
+  packageName        : 'my.test package#name',
+  packageTitle       : 'Super Duper Package',
+  packageVersion     : '0.1.0.0',
+  packageType        : 'Custom',
+      
+  packageAuthors     : 'Chevdor',
+  packageOwners      : 'Chevdor',
+      
+  packageSummary     : 'Some blabla',
+  packageDescription : 'Some other blabla',
+  readmeFormat       : 'Asciidoc',
+  packageProjectURL  : 'http://www.someurl.com',
+  packageTags        : 'tag1, tag2',
+      
+  licenseRequired    : 'N',    
+  packageLicenseURL  : 'http://license.org',  
+  packageCopyright   : 'Some more blabla'
+}
+
 describe('chocolatey generator', function () {
   beforeEach(function (done) {
     helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
@@ -20,13 +41,30 @@ describe('chocolatey generator', function () {
   it('creates expected files', function (done) {
     var expected = [
       // add files you expect to exist here.
-      'undefined.nuspec',
+      testData.packageName+'.nuspec',
       'tools/chocolateyInstall.ps1',
       'tools/chocolateyUninstall.ps1'
     ];
 
     helpers.mockPrompt(this.app, {
-      'someOption': true
+      'githubUser'         : testData.githubUser,
+      'packageName'        : testData.packageName,
+      'packageTitle'       : testData.packageTitle,
+      'packageVersion'     : testData.packageVersion,
+      'packageType'        : testData.packageType,
+      
+      'packageAuthors'     : testData.packageAuthors,
+      'packageOwners'      : testData.packageOwners,
+      
+      'packageSummary'     : testData.packageSummary,
+      'packageDescription' : testData.packageDescription,
+      'readmeFormat'       : testData.readmeFormat,
+      'packageProjectURL'  : testData.packageProjectURL,
+      'packageTags'        : testData.packageTags,
+        
+      'licenseRequired'    : testData.licenseRequired,  
+      'packageLicenseURL'  : testData.packageLicenseURL,
+      'packageCopyright'   : testData.packageCopyright
     });
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
